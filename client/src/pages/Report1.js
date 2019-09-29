6ae894be8f5252ba7eb8b830e7e4bddfe36bc337
Thomas Lewis <tplewis@gmail.com>
2019-09-29 15:17:53 -0400

    added Report1.js

--- client/src/pages/Report1.js
+++ client/src/pages/Report1.js
@@ -5,6 +5,7 @@ import React, { Component } from "react";
 // import { List, ListItem } from "../components/List";
 // import { Input, FormBtn } from "../components/Form";
 
+
 class Report1 extends Component {
     render() {
         return(
