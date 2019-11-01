68ea0fb71a34b8d428b869e723b1c663c33c6448
Thomas Lewis <tplewis@gmail.com>
2019-11-01 06:39:55 -0400

    attempting to get heroku to work

---
+++ client/build/precache-manifest.0b52c8d555bb75f90ba0335503304604.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab670b84e159823706cc",
+    "url": "/static/js/main.41b3189f.chunk.js"
+  },
+  {
+    "revision": "b9a979ff77452797278a",
+    "url": "/static/js/2.eeb7b399.chunk.js"
+  },
+  {
+    "revision": "ab670b84e159823706cc",
+    "url": "/static/css/main.949defe9.chunk.css"
+  },
+  {
+    "revision": "8280e2a565969b3dc5164bb5f1da747e",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,10 +12,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+//app.use(express.static(path.join(__dirname, "client", "build")))
+
+// how-to says use code above while code below is existing and looks good also, which is it?
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 
 // Add routes, both API and view
 app.use(routes);
@@ -25,6 +31,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/familyreunion",
 { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true,
   useFindAndModify: false }
 );
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // Start the API server
 app.listen(PORT, function() {
